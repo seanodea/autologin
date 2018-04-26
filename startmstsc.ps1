4ae39523b99d727d29a8efc0f018f385a5e08f2e
Godwin Christopher <christopher.godwin@igt.com>
2018-04-26 13:15:14 -0500

    sudo pw now on if missing from yaml

--- startmstsc.ps1
+++
@@ -1,5 +0,0 @@
-param([string]$username = "username", [string]$password = "password", [string]$server = "server")
-Write-Host "Arg: $username"
-Write-Host "Arg: $password"
-cmdkey /generic:DOMAIN/$server /user:$username /pass:$password
-mstsc.exe Default.rdp /v $server;
